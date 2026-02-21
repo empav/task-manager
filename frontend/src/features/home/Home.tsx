591a8f0b4dd12217e4119a60860bae3f1d54d30a
Emanuele Pavan <emanuele.pav@gmail.com>
2026-02-21 18:22:45 +0100

    protects home if the user is not logged in

--- frontend/src/features/home/Home.tsx
+++ frontend/src/features/home/Home.tsx
@@ -1,4 +1,6 @@
 import { Layout } from "antd";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import TaskTable from "./TaskTable";
 import Title from "./Title";
@@ -7,6 +9,15 @@ import { useCountTasksQuery } from "../../hooks";
 
 export default function Home() {
   const { data: taskCount = 0, isLoading } = useCountTasksQuery();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Making sure that if the user tries to access /home without a token, they are redirected to /login
+    const token = sessionStorage.getItem("auth_token");
+    if (!token) {
+      navigate("/login", { replace: true });
+    }
+  }, [navigate]);
 
   return (
     <Layout.Content className="home-content">
